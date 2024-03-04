020f4ccb13ee48560e614b40fde47086bab52601
Rachel Sanford <rachelsa@va-g5.kzn.21-school.ru>
2024-03-04 09:13:40 +0300

    added checker for invalid input size of matrix

--- backend/services/matrix_calculator/srcs/Transformation.cpp
+++ backend/services/matrix_calculator/srcs/Transformation.cpp
@@ -5,6 +5,9 @@
 #include "Matrix.h"
 
 Matrix Matrix::Transpose() {
+    if (!this->CheckRowsCols()) {
+        throw std::invalid_argument("Rows (cols) are invalid. They must be positive integers >= 0.\n");
+    }
     Matrix result(cols_, rows_);
     for (int i = 0; i < rows_; ++i) {
         for (int j = 0; j < cols_; ++j) {
